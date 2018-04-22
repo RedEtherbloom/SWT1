d54e44dd8b4bccc5072b36540225bca603e0d8d0
GityUpNow <kim-cheng@gmx.de>
2018-04-22 15:20:34 +0200

    Added 90 and 270 degree-compare functions. Added a image-cloner

--- iMage/jmjrst.main/src/test/java/edu/kit/ipd/swt1/GeneralTestsHelpers.java
+++ iMage/jmjrst.main/src/test/java/edu/kit/ipd/swt1/GeneralTestsHelpers.java
@@ -1,6 +1,8 @@
 package edu.kit.ipd.swt1;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 
@@ -27,6 +29,18 @@ public final class GeneralTestsHelpers {
 	}
 	
 	/**
+	 * @param image Buffered image to clone
+	 * @return Returns a deep copy of the image
+	 */
+	public static BufferedImage cloneImage(BufferedImage image) {
+		 ColorModel color = image.getColorModel();
+		 boolean premultiplied = color.isAlphaPremultiplied();
+		 WritableRaster raster = image.copyData(null);
+		 
+		 return new BufferedImage(color, raster, premultiplied, null);
+		}
+	
+	/**
 	 * @param a First {@link BufferedImage}
 	 * @param b Second {@link BufferedImage}
 	 * @return Returns whether they are equal (point to the same Object, or match in pixels)
@@ -46,6 +60,54 @@ public final class GeneralTestsHelpers {
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
 				if (a.getRGB(i, j) != b.getRGB(i, j)) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * @param a Normal buffered Image
+	 * @param b By 90 degrees rotated Image
+	 * @return Returns if the they both have the same pixeladata
+	 */
+	public static boolean sameImageAfter90(BufferedImage a, BufferedImage b) {
+		int height = a.getHeight();
+		int width  = a.getWidth();
+		
+		if (height != b.getWidth() || width != b.getHeight()) {
+			return false;
+		}
+		
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if (a.getRGB(i, j) != b.getRGB(height - j, i)) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * @param a Normal buffered Image
+	 * @param b By 270 degrees rotated Image
+	 * @return Returns if the they both have the same pixeladata
+	 */
+	public static boolean sameImageAfter270(BufferedImage a, BufferedImage b) {
+		int height = a.getHeight();
+		int width  = a.getWidth();
+		
+		if (height != b.getWidth() || width != b.getHeight()) {
+			return false;
+		}
+		
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				if (a.getRGB(i, j) != b.getRGB(j, width - i)) {
 					return false;
 				}
 			}
